02083cde067cf5ed906cf1defc6d76751d63b835
dpudpu <dpudpu11@gmail.com>
2020-05-06 05:03:16 +0900

    fix parameter store

--- src/main/java/com/example/aws/model/parameterstore/ParameterStoreController.java
+++ src/main/java/com/example/aws/model/parameterstore/ParameterStoreController.java
@@ -1,20 +1,18 @@
 package com.example.aws.model.parameterstore;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
 @RestController
 @RequestMapping("/webapi/v1/params-store")
 public class ParameterStoreController {
     @Value("${username}")
-    private final String username;
+    private String username;
     @Value("${password}")
-    private final String password;
+    private String password;
 
     @GetMapping
     public ResponseEntity<String> param() {
